8ec160a004ff38a761f59eb827b7e6356b1a2566
Anna (Yinqi) Zhang <39491815+AnnaZhang-JMK@users.noreply.github.com>
2018-11-05 15:10:23 -0500

    Project Setup

---
+++ project.R
@@ -0,0 +1,24 @@
+install.packages("gmailr")
+library('gmailr')
+use_secret_file('stat463.json')
+
+sender = "psu.forecasting.group.3@gmail.com"
+receiver = "psu.forecasting.group.3@gmail.com"
+
+send_message(mime(
+  To = receiver,
+  From = sender,
+  Subject = "Hi",
+  body = "Hello"))
+
+install.packages('openssl')
+library(openssl)
+
+library(pageviews)
+library(simts)
+wiki_ts = project_pageviews(granularity = "daily", start = "2018090100", end = "2018102100")
+
+Xt = gts(wiki_ts$views)
+plot(Xt)
+
+gts_time(gts(wiki_ts$views))
